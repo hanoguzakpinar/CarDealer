a47fa0fae046b99606fcce5ae8a4ccc47f555b3c
hanoguzakpinar <hanoguzakpinar@gmail.com>
2025-11-23 14:45:19 +0300

    baseEntity ve Car eklendi

---
+++ CarDealer.Domain/Common/BaseEntity.cs
@@ -0,0 +1,7 @@
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime? UpdatedDate { get; set; }
+    public bool IsDeleted { get; set; }
+}
---
+++ CarDealer.Domain/Entities/Car.cs
@@ -0,0 +1,8 @@
+public class Car : BaseEntity
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+    public string Color { get; set; }
+}
